ad69a19df4f2f99dd77bf5c96f22876779175ba2
mstocc2 <mstocc2@illinois.edu>
2022-03-13 01:52:49 -0600

    ESTABLISHED SERVER SIDE CSV UPDATES

--- mongon_api.py
+++ mongon_api.py
@@ -1,7 +1,8 @@
 # mongon_api.py
 # This program is supposed to set up an api to communicate with stream elements
 # This api needs to communicate with 'mongon_chat_history.csv'
-# inspired by https://programminghistorian.org/en/lessons/creating-apis-with-python-and-flask
+# This build only lanches a local API with http://127.0.0.1:5000/
+# This program only mimics the server API (path definition is different)
 # imports
 import flask
 from flask import request
@@ -9,23 +10,37 @@ import pandas as pd
 import threading
 import sys
 import os
-import time
+import csv
+import ast
 
 
-# link: http://127.0.0.1:5000/
-
+# api initialization using flask
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-
+# csv file and gobal variables initialiation
 path = r"C:\Users\socra\Documents\git-hut_stuff\twitch_interaction_sesaw_project\Twitch_Integration_Project\mongon_chat_history.csv"
 escaper = None
+header = ['dt', 'channel', 'username', 'message']
 
+
+# created escape loop for debugging
 def escape_route():
     global escaper
     while escaper != "Y":
         escaper = str(input("Exit now? Enter \'Y\': \n")).strip()
     os._exit(os.X_OK)
+
+
+# api GET routes created for stream 
+
+def commands():
+    
+    @app.route('/commands', methods=['GET'])
+    def commandget():
+        return '''Mongon's custom commands: 
+        !wordcount [username] [word], !amoguscount [username].
+'''
 
 
 def amogusfinder():
@@ -40,7 +55,7 @@ def amogusfinder():
             return "{no name!}"
         try:
             dfusername = dfchat[(dfchat["username"] == name)]
-            dfstring = dfusername[dfusername["message"].str.contains('|'.join(variants), 
+            dfstring = dfusername[dfusername["message"].str.contains('|'.join(variants),
                 na=False, case=False, regex=True)]
             return str(dfstring["message"].count())
         except:
@@ -59,11 +74,11 @@ def susfinder():
             return "{no name!}"
         try:
             dfusername = dfchat[(dfchat["username"] == name)]
-            dfstring = dfusername[dfusername["message"].str.contains('|'.join(variants), 
+            dfstring = dfusername[dfusername["message"].str.contains('|'.join(variants),
                 na=False, case=False, regex=True)]
             return str(dfstring["message"].count())
-        except Exception as e:
-            return f"ERROR Mongon FUCKED UP HIS PROGRAM (failed look-up)"
+        except Exception:
+            return "ERROR Mongon FUCKED UP HIS PROGRAM (failed look-up)"
 
 
 def bakafinder():
@@ -77,7 +92,7 @@ def bakafinder():
             return "{no name!}"
         try:
             dfusername = dfchat[(dfchat["username"] == name)]
-            dfstring = dfusername[dfusername["message"].str.contains('|'.join(variants), 
+            dfstring = dfusername[dfusername["message"].str.contains('|'.join(variants),
                 na=False, case=False, regex=True)]
             return str(dfstring["message"].count())
         except:
@@ -98,26 +113,53 @@ def wordfinder():
             word = str(request.args['word'])
         else:
             return "{no word!}"
-            
+
         try:
             dfusername = dfchat[(dfchat["username"] == name)]
-            dfstring = dfusername[dfusername["message"].str.contains(word, 
+            dfstring = dfusername[dfusername["message"].str.contains(word,
                 na=False, case=False, regex=True)]
             return str(dfstring["message"].count())
         except:
             return "{ERROR Mongon FUCKED UP HIS PROGRAM (failed look-up)}"
 
 
+# api POST routes designed to receive chat messages from twitch_cat_receiver.py
+
+def update_online_csv():
+    @app.route('/respdict', methods=['POST'])
+    def wordreceive(): 
+
+        # converting binary stringb back to dict
+        d = request.get_data()
+        d = d.decode("UTF-8")
+        d = ast.literal_eval(d)
+
+        # writing to server's csv file
+        with open(path, 'a', newline='', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames = header)
+            writer.writerow(d) 
+
+        return d
+
+
+# main method. could not define it at beginnig of file because of @app.route commands
 def main():
-    global dfchat
     # escape_route_thread = threading.Thread(target = escape_route)
     # escape_route_thread.start()
+
+    # get requests
+    commands()
     amogusfinder()
     susfinder()
     bakafinder()
     wordfinder()
-    app.run()
+
+    # post requests
+    update_online_csv()
+
+    # starting api
+    if __name__ == '__main__':
+        app.run()
 
 
 main()
-
--- twitch_chat_receiver.py
+++ twitch_chat_receiver.py
@@ -1,7 +1,7 @@
-# twitch_chat_reciver
-# This file takes chat logs from twitch.tv/m0ng0n and analyzes them
+# twitch_chat_reciver.py
+# This file takes chat logs from twitch.tv/m0ng0n and stores them
 #
-# Leared from this: 
+# Learned from this: 
 # https://www.learndatasci.com/tutorials/how-stream-text-data-twitch-sockets-python/
 #
 #imports
@@ -15,7 +15,10 @@ import csv
 import threading
 import sys
 import os
+import requests
 
+
+# setting gobal variables
 sock = socket.socket()
 server = 'irc.chat.twitch.tv'
 port = 6667
@@ -23,22 +26,30 @@ nickname = 'Mongon'
 token = None
 channel = '#m0ng0n'
 
-with open(r"C:\Users\socra\Desktop\twitchauth.txt","r") as file:
-    token = file.readlines()[0]
+# grabbing private api POST link and stream chat oath ticket
+try:
+    with open(r"C:\Users\socra\Documents\git-hut_stuff\DO_NOT_CLONE_THIS_FILE\authtickets.txt","r") as file:
+        lines = file.readlines()
+        token = lines[0]
+        resplink = lines[1]
+except Exception as e:
+    print(e)
 
 infile = None
 escaper = None
+csvpath = 'mongon_chat_history.csv'
 
+
+# main execution
 def main():
 
     #escape_route_thread = threading.Thread(target = escape_route)
     #escape_route_thread.start()
     initialize_csv_file()
-    #recieving_twitchchat_thread = threading.Thread(target = sock_to_twitch)
-    #recieving_twitchchat_thread.start()
     sock_to_twitch()
     
 
+# connecting to twitch on  a loop until success
 def sock_to_twitch():
     while True:
         try:
@@ -49,34 +60,41 @@ def sock_to_twitch():
             sock.send(f"JOIN {channel}\n".encode('utf-8'))
             break
         except Exception as ConnectionResetError:
-            print("Connection Error... Retrying\n")
-            continue
+            print("Connection Error... Retrying...\n")
+
+    # starting twitch chat collection
     receivingtwitchchat()
     
 
-
+# receives twitch chat
 def receivingtwitchchat():
     while True:
         resp = sock.recv(2048).decode('utf-8').strip()
         print(resp) # remove print     
-        sorting_each_chat(resp)
-        
 
+        # sorts twitch chat and stores it into csv file and onine server
+        sorting_each_chat(resp)
+    
+
+# opening csv file to store chat in locally
 def initialize_csv_file():
     global infile
     try:
-        infile = open('mongon_chat_history.csv', 'r', encoding='UTF8')
+        infile = open(csvpath, 'r', encoding='UTF8')
     except FileNotFoundError:
-         infile = open('mongon_chat_history.csv', 'w', encoding='UTF8')
+         infile = open(csvpath, 'w', encoding='UTF8')
          headers = ['dt', 'channel', 'username', 'message']
          dfchat = pd.DataFrame(columns = headers)
-         dfchat.to_csv('mongon_chat_history.csv', index=False)
+         dfchat.to_csv(csvpath, index=False)
     
 
 
+# sorting twitch chat into 4 categories the storing in locally and online
 def sorting_each_chat(resp):
     header = ['dt', 'channel', 'username', 'message']
     try:
+
+        # Chicago time zone for dating chat
         tz_CH = pytz.timezone('America/New_York') 
         datetime_CH = datetime.now(tz_CH)
         time_logged = datetime_CH.strftime("%Y-%m-%d_%H:%M:%S")
@@ -86,7 +104,15 @@ def sorting_each_chat(resp):
         ).groups()
         
         d = {'dt': time_logged, 'channel': channel, 'username': username, 'message': message}
-        with open('mongon_chat_history.csv', 'a', newline='', encoding='utf-8') as file:
+    
+        # sending dict in string form to server
+        post = requests.post(resplink, data = str(d))
+
+        # checking post success
+        print(post)
+
+        # appending dict to local csv file
+        with open(csvpath, 'a', newline='', encoding='utf-8') as file:
 
             writer = csv.DictWriter(file, fieldnames = header)
             writer.writerow(d)  
@@ -94,7 +120,8 @@ def sorting_each_chat(resp):
         pass
         
 
-
+# extra loop for debugging. 
+# This method seems to lag the program when running so it is commented out by default
 def escape_route():
     global infile
     global escaper
